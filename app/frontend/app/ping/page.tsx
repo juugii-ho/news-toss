84aa16ed37f03bded9c2692e50a1eb2adc13c0ea
juugii-ho <77222382+juugii-ho@users.noreply.github.com>
2025-12-01 02:56:58 +0900

    Add /ping route for debugging Vercel deployment

---
+++ app/frontend/app/ping/page.tsx
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic';
+
+export default function PingPage() {
+    return (
+        <div>
+            <h1>Pong</h1>
+            <p>System is online.</p>
+        </div>
+    );
+}
